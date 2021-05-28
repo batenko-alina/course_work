a015ee86746629d5c30c009888887b52facdba7c
Alexander Antonenko <asantonenko@gmail.com>
2021-05-28 18:52:02 +0300

    Исправлена циклическая ссылка в Person.h
    Исправлена циклическая ссылка в заголовочных файлах - вместо #include "Organization.h" используется class Organization;
    исправлен #include<> на  #include"" при подключении заголовочных файлов из проекта

--- include/Person.h
+++ include/Person.h
@@ -1,13 +1,16 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#include <Organization.h>
-#include <PhoneBookItem.h>
+//#include "Organization.h" - циклическая ссылка в h файлах невозможна
+#include "PhoneBookItem.h"
+
+class Organization; //прототип класса - решает проблему циклической ссылки
+
 class Person:PhoneBookItem
 {
     public:
         Person();
-        virtual ~Person();
+        virtual ~Person(); //вообще-то виртуальный деструктор нужно и достаточно сделать в базовом классе
 
         string GetFIO();
         void SetFIO(string val);
@@ -20,7 +23,7 @@ class Person:PhoneBookItem
 
     private:
     string FIO;
-    enum sex;
+    enum sex; //неверно - вначале объявите тип enum (например, enum Sex {male, female};), а здесь объявите поле данного типа
     Organization* organization;
 
 };
