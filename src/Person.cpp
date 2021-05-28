ed4ee4e4114e5495f09af409b34c63f05a5dcc36
Alexander Antonenko <asantonenko@gmail.com>
2021-05-28 19:03:49 +0300

    Update Person.cpp

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,5 +1,9 @@
 #include "Person.h"
 
+#include "Organization.h" 
+//мы удалили эту строку из Person.h и я, на всякий случай, её указал в этом файле
+
+//не нужно дублировать содержимое h файла в cpp файле
 class Person:PhoneBookItem
 {
     public:
